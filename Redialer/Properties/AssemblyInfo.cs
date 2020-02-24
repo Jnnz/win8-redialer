b06e4b0d3c7e8323e783ea1b3f06a074152e2363
Johann Perozo <johann.prz@gmail.com>
2020-02-24 14:05:56 -0400

    Updates AssemblyInfo

--- Redialer/Properties/AssemblyInfo.cs
+++ Redialer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Win8Redialer")]
+[assembly: AssemblyTitle("Redialer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ankit Sharma")]
-[assembly: AssemblyProduct("Win8Redialer")]
-[assembly: AssemblyCopyright("Copyright ©  2013-14")]
+[assembly: AssemblyCompany("Johann Perozo")]
+[assembly: AssemblyProduct("Redialer")]
+[assembly: AssemblyCopyright("Copyright ©  2013-14 Ankit Sharma, 2020 Johann Perozo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.12")]
-[assembly: AssemblyFileVersion("0.12")]
+[assembly: AssemblyVersion("1.13")]
+[assembly: AssemblyFileVersion("1.13")]
