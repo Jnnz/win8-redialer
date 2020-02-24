461194d61ea2aaf8bc4dceacaa03d24d37693615
Johann Perozo <johann.prz@gmail.com>
2020-02-24 16:01:24 -0400

    Updates assemblyinfo

--- Redialer/Properties/AssemblyInfo.cs
+++ Redialer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Redialer")]
-[assembly: AssemblyCopyright("Copyright ©  2013-14 Ankit Sharma, 2020 Johann Perozo")]
+[assembly: AssemblyCopyright("Copyright © 2020 Johann Perozo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.13.1")]
-[assembly: AssemblyFileVersion("1.13.1")]
+[assembly: AssemblyVersion("1.13.2")]
+[assembly: AssemblyFileVersion("1.13.2")]
